51f6603418bcc97661750cf5c88b92dfd68b22df
Jannes <jannesbr@pik-potsdam.de>
2024-11-27 21:11:02 +0100

    adjust for pycopanlpjml, new structure with base component and individualized entities, also using component/model class

--- inseeds/components/farmer_management/world.py
+++ inseeds/components/farmer_management/world.py
@@ -13,8 +13,10 @@
 
 from . import documentation as doc
 
+import inseeds.components.base as base
 
-class World(doc.World):
+
+class World(base.World, doc.World):
     """Define properties.
     Inherits from I.World as the interface with all necessary variables
     and parameters.
@@ -22,29 +24,7 @@ class World(doc.World):
 
     def __init__(self, **kwargs):
         """Initialize an instance of World."""
-        super(World, self).__init__(**kwargs)
-
-    def init_farmers(self, **kwargs):
-        """Initialize farmers."""
-        cells = self.init_cells(**kwargs)
-        farmers = []
-
-        for cell in cells:
-            if cell.output.cftfrac.sum("band") == 0:
-                continue
-
-            farmer = self.model.Farmer(
-                cell=cell, config=self.lpjml.config.coupled_config
-            )
-            farmers.append(farmer)
-
-        farmers_sorted = sorted(farmers, key=lambda farmer: farmer.avg_hdate)
-        for farmer in farmers_sorted:
-            farmer.init_neighbourhood()
-
-        self.update_output_table(self.lpjml.sim_year - 1, init=True)
-
-        return farmers_sorted, cells
+        super().__init__(**kwargs)
 
     @property
     def farmers(self):
@@ -55,14 +35,3 @@ class World(doc.World):
             if farmer.__class__.__name__ == "Farmer"  # noqa
         }
         return farmers
-
-    def update_farmers(self, t):
-        farmers_sorted = sorted(self.farmers, key=lambda farmer: farmer.avg_hdate)
-        for farmer in farmers_sorted:
-            farmer.update_behaviour(t)
-
-    def update(self, t):
-        self.update_farmers(t)
-        self.update_output_table(t)
-
-        self.update_lpjml(t)
